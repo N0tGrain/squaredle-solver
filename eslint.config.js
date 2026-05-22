8e3ba43ff1ce649192d1bf5832e39e0d0d35a93f
Kaylee <s1152274@student.hsleiden.nl>
2026-05-22 20:22:34 +0200

    Hotfix: updated eslint project setup

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import globals from 'globals'
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import pluginOxlint from 'eslint-plugin-oxlint'
+import tsparser from '@typescript-eslint/parser'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig([
   {
@@ -14,10 +17,28 @@ export default defineConfig([
 
   {
     languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      },
       globals: {
         ...globals.browser,
       },
     },
+    plugins: {
+      vue,
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'no-console': 'off',
+    },
+    extends: [
+      'plugin:vue/vue3-recommended',
+      'plugin:@typescript-eslint/recommended',
+    ]
   },
 
   js.configs.recommended,
